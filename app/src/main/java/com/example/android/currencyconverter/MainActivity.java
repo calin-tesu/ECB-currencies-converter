cdc791b5dd9c4acac6f6c93368ffbb5b63dac8b5
calin-tesu <likareturneanu@gmail.com>
2019-08-28 18:23:12 +0300

    Get the exchange rate xml file from ECB Save the file in Internal Storage

---
+++ app/src/main/java/com/example/android/currencyconverter/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.android.currencyconverter;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String EXCHANGE_RATES_XML = "exchange_rates.xml";
+
+    private Button checkButton;
+    private TextView textView;
+
+    File file;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkButton = findViewById(R.id.checkButton);
+        textView = findViewById(R.id.text);
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkForFile();
+            }
+        });
+    }
+
+    private void checkForFile() {
+
+        file = getBaseContext().getFileStreamPath(EXCHANGE_RATES_XML);
+
+        if (!file.exists()) {
+            Toast.makeText(getApplicationContext(), "Start download!!", Toast.LENGTH_SHORT).show();
+            new DownloadExchangeRatesFromECB().execute();
+        } else {
+            Toast.makeText(getApplicationContext(), "File allready exist!", Toast.LENGTH_SHORT).show();
+            String xml = readFromFile(getApplicationContext());
+            textView.setText(xml);
+        }
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(EXCHANGE_RATES_XML);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    class DownloadExchangeRatesFromECB extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            int count;
+            try {
+                URL url = new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml?ecf1fe18196a2eeb131d673c7c204355");
+
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(),
+                        8192);
+
+                // Output stream
+                FileOutputStream output = openFileOutput(EXCHANGE_RATES_XML, Context.MODE_PRIVATE);
+
+                //OutputStream output = new FileOutputStream(EXCHANGE_RATES_XML);
+
+                byte[] data = new byte[1024];
+
+                //long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    //total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    //publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getApplicationContext(),"Download successful!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
